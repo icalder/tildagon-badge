4ceb18d6607cd225f4d9233042b349b6704e5bcf
Iain Calder <iain.calder@gmail.com>
2026-04-02 20:41:56 +0000

    feat: added basic template app

---
+++ embassy_hello_world/src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_backtrace as _;
+use tildagon::hardware::TildagonHardware;
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[embassy_executor::task]
+async fn hello_world_task() {
+    loop {
+        esp_println::println!("Hello World");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+}
+
+#[esp_rtos::main]
+async fn main(_spawner: Spawner) {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let _hardware = TildagonHardware::new(peripherals)
+        .await
+        .expect("Tildagon hardware init failed");
+
+    _spawner.spawn(hello_world_task()).expect("spawn hello_world_task");
+    loop {
+        Timer::after(Duration::from_secs(60)).await;
+    }
+}
